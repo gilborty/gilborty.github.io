b7710c1a662c584ecaf0004cfb476528713eb41b
Gilbert Montague <gilbert@missionrobotics.us>
2024-05-23 11:35:30 -0700

    don't use a cdn

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
 // Basic Three.js setup
+import * as THREE from '../libs/three.module.min.js';
+
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 const renderer = new THREE.WebGLRenderer();
